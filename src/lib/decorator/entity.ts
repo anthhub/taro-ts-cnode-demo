3c0c08f331798356c45ac040da9ee4429259e283
anthhub <andhub@163.com>
2019-03-17 11:31:04 +0800

    entity modify

--- src/lib/decorator/entity.ts
+++ src/lib/decorator/entity.ts
@@ -10,14 +10,13 @@ class EntityBase<T> {
 /**
  *  实体类装饰器, 自动构建类属性, 构造函数空参赋值null
  */
-
 function entity<T extends new (...args: any[]) => {}>(constructor: T) {
     const thefieldStore = fieldStore
     fieldStore = {}
 
     return class extends constructor {
         constructor(...props) {
-            super()
+            super(...props)
             const prop = props[0]
             Object.keys(thefieldStore).map(item => (this[item] = prop ? prop[thefieldStore[item]] : null))
             fieldStore = {}
@@ -28,7 +27,6 @@ function entity<T extends new (...args: any[]) => {}>(constructor: T) {
 /**
  * 字段别名装饰器,  优先级最高
  */
-
 function fieldName(param?: string) {
     return (_target, key) => {
         fieldStore[key] = param || key
@@ -38,7 +36,6 @@ function fieldName(param?: string) {
 /**
  * 字段装饰器,  声明字段
  */
-
 function field(_target, key) {
     fieldStore[key] = key
 }
